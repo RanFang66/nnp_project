cc2523ce1eb0f29d1db967e510b8fc7426105118
ranfang <ranfang66@gmail.com>
2021-04-13 10:27:28 +0800

    Init the dsp2833x project

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+*********************************************************************************************************
+*                   Copyright @2021 BeiJing AenoMed Co.,Ltd. All rights reserved.
+*
+* File Name : 		main.c
+* Author : 			Fang Ran
+* Created Date : 	2021.04.13
+* Modified Date : 	2021.04.13
+* Description :
+* Version : 		V1.0.0 20210413Alpha
+* Others :
+*
+*********************************************************************************************************
+*/
+
+int main(void)
+{
+	return 0;
+}
